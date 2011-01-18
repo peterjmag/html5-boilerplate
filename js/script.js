74b24de484efa396e06b039fd480fe6c57af4236
Peter Magenheimer <peter.magenheimer@gmail.com>
2011-01-17 19:17:39 -0700

    Experiment with jQuery cycle fade transitions.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@
 
 $(document).ready(function(){
   $.localScroll({axis:'yx',queue:'true',hash:'true'});
+  $('#test-slider').cycle({
+    fx: 'fade',
+    timeout: 0,
+    next: 'a.slider-next'
+  });
 });
+
+// $(function() {
+//   $('#slider').cycle('scrollUp');
+// }
+// 
+
 
 
 
