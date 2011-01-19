f2fdf84b847249615e263a81f6827b24d16f3f43
Peter Magenheimer <peter.magenheimer@gmail.com>
2011-01-18 17:33:28 -0700

    Allow multiple slideshows on one page with each().

--- js/script.js
+++ js/script.js
@@ -4,18 +4,22 @@
 $(document).ready(function(){
   $.localScroll({axis:'yx',queue:'true',hash:'true'});
 
-  $('.slides').before('<ul class="slides-nav">').cycle({
-    fx: 'fade',
-    timeout: 0,
-    next: ".slides",
-    pager: ".slides-nav",
-    activePagerClass: 'active-slide',
+  $('.slides').each(function() {
+    var p = this.parentNode;
 
-    // callback fn that creates a thumbnail to use as pager anchor
-    pagerAnchorBuilder: function(idx, slide) {
-      return '<li><a href="#"><img src="' + slide.src + '" width="140" /></a></li>';
-    }
-  });
+    $(this).before('<ul class="slides-nav">').cycle({
+      fx: 'fade',
+      timeout: 0,
+      next: $(".slides", p),
+      pager: $(".slides-nav", p),
+      activePagerClass: 'active-slide',
+
+      // callback fn that creates a thumbnail to use as pager anchor
+      pagerAnchorBuilder: function(idx, slide) {
+        return '<li><a href="#"><img src="' + slide.src + '" width="140" /></a></li>';
+      }
+    });
+  })
 });
 
 
